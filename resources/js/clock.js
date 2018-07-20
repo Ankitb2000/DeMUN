d29759f26d28230b2d0b2c2a616c3d2e167a4662
Ankitb2000 <41199612+Ankitb2000@users.noreply.github.com>
2018-07-21 00:05:49 +0530

    Update clock.js

--- resources/js/clock.js
+++ resources/js/clock.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = 'August 2 2018 08:00:00 GMT+0530';
+var deadline = 'August 2 2018 08:30:00 GMT+0530';
 initializeClock('clockdiv', deadline);
